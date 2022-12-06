7aec4bfd2f7f53ffdcbc063ba3600cca437bced1
SofiiaShevchenko <118025665+SofiiaShevchenko@users.noreply.github.com>
2022-12-06 22:12:29 +0200

    Add files via upload

---
+++ 11_2  1(1).cpp
@@ -0,0 +1,20 @@
+﻿// 11_2  1(1)
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+    const int size = 10;
+    int arr[size] = { 1,2,0,24,-12,-3,12,56,-22,37 };
+
+    sort(begin(arr), end(arr));
+
+    cout << arr[1] << endl;
+
+}
+
---
+++ 11_2  1(2).cpp
@@ -0,0 +1,47 @@
+﻿// 11_2  1(2)
+
+
+#include <iostream>
+#include <algorithm>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Масив:";
+    const int m = 15;
+
+    int arr[m];
+
+    for (int i = 0; i < m; i++)
+
+    {
+
+        arr[i] = rand() % 3;
+
+        cout << arr[i] << " ";
+
+    }
+
+    cout << "" << endl;
+
+    cout << "Впорядкований масив:";
+
+    for (int i = 0; i < m; i++)
+
+    {
+
+        arr[i] = rand() % 3;
+
+        sort(begin(arr), end(arr));
+
+        cout << arr[i] << " ";
+
+    }
+
+}
